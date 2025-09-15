40a1295567f8e81a766d2d9ea7ae9b531a8a65cb
이정준 <ljj10571@gmail.com>
2025-09-15 15:40:17 +0900

    [Feat] create TeamQueryPort

---
+++ src/main/java/com/jikchin/jikchin_app/application/port/out/team/KLeagueTeamQueryPort.java
@@ -0,0 +1,5 @@
+package com.jikchin.jikchin_app.application.port.out.team;
+
+public interface KLeagueTeamQueryPort {
+    boolean existsById(Integer teamId);
+}
---
+++ src/main/java/com/jikchin/jikchin_app/application/port/out/team/KboTeamQueryPort.java
@@ -0,0 +1,5 @@
+package com.jikchin.jikchin_app.application.port.out.team;
+
+public interface KboTeamQueryPort {
+    boolean existsById(Integer teamId);
+}
