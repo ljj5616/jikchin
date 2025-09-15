50f4306bf314c737317774f338a6f82a362084ca
이정준 <ljj10571@gmail.com>
2025-09-15 15:29:40 +0900

    [Chore] insert Bean with @Service

--- src/main/java/com/jikchin/jikchin_app/application/port/out/user/LoadUserPort.java
+++
@@ -1,9 +0,0 @@
-package com.jikchin.jikchin_app.application.port.out.user;
-
-import com.jikchin.jikchin_app.domain.user.model.User;
-
-import java.util.Optional;
-
-public interface LoadUserPort {
-    Optional<User> loadById(Long id);
-}
