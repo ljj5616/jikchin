fdc17ebeb4b13d0ed17856ab1b85881c0009f095
이정준 <ljj10571@gmail.com>
2025-09-15 15:10:40 +0900

    [Feat] create CompleteProfileUseCase

---
+++ src/main/java/com/jikchin/jikchin_app/application/port/in/auth/CompleteProfileUseCase.java
@@ -0,0 +1,8 @@
+package com.jikchin.jikchin_app.application.port.in.auth;
+
+import com.jikchin.jikchin_app.application.dto.auth.CompleteProfileRequest;
+import com.jikchin.jikchin_app.application.dto.auth.CompleteProfileResponse;
+
+public interface CompleteProfileUseCase {
+    CompleteProfileResponse completeProfile(Long userId, CompleteProfileRequest request);
+}
