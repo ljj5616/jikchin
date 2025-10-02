2dd10aa23569fc9e6a7cc5af1bd7606095a82604
이정준 <ljj10571@gmail.com>
2025-10-02 16:10:32 +0900

    [Chore] delete unusable lines

--- src/main/java/com/jikchin/jikchin_app/application/port/in/profile/CompleteProfileUseCase.java
+++ src/main/java/com/jikchin/jikchin_app/application/port/in/profile/CompleteProfileUseCase.java
@@ -1,8 +1,5 @@
 package com.jikchin.jikchin_app.application.port.in.profile;
 
-import com.jikchin.jikchin_app.adapter.in.web.profile.dto.CompleteProfileHttpRequest;
-import com.jikchin.jikchin_app.adapter.in.web.profile.dto.CompleteProfileHttpResponse;
-
 public interface CompleteProfileUseCase {
     CompleteProfileResult completeProfile(CompleteProfileCommand command);
 }
